f1ec78e99830781f6e5ef236152b9d18621a0ac7
Jason Plumb <jplumb@splunk.com>
2021-11-09 16:23:35 -0800

    setup

---
+++ src/main/java/com/splunk/lambdas/Main.java
@@ -0,0 +1,42 @@
+package com.splunk.lambdas;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+
+    private void run() throws Exception {
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+        while(true){
+            System.out.print("Running 2 jobs...");
+            CompletableFuture<?> fooFuture = CompletableFuture.runAsync(this::foo, exec);
+            CompletableFuture<?> barFutureFuture = CompletableFuture.runAsync(this::foo, exec);
+            CompletableFuture.allOf(fooFuture, barFutureFuture).get();
+            System.out.println("complete.");
+        }
+    }
+
+
+    void foo() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void bar() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
